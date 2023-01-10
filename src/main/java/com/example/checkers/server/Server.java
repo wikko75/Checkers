b871040de2607beb4821eadd74e5af61173c8f09
JanNawrat <jan.ryszard.nawrat@gmail.com>
2023-01-10 18:22:29 +0100

    Limited the number of BufferedReader and PrintWriter instances on server side

--- src/main/java/com/example/checkers/server/Server.java
+++ src/main/java/com/example/checkers/server/Server.java
@@ -6,9 +6,9 @@ import java.net.Socket;
 
 public class Server {
 
-    private static void startGame(Socket firstPlayer, Socket secondPlayer, VariantBuilder variantBuilder) {
+    private static void startGame(BufferedReader inF, BufferedReader inS, PrintWriter outF, PrintWriter outS, VariantBuilder variantBuilder) {
         VariantDirector director = new VariantDirector(variantBuilder);
-        GameInstance gameInstance = new GameInstance(firstPlayer, secondPlayer, director.getVariant());
+        GameInstance gameInstance = new GameInstance(inF, inS, outF, outS, director.getVariant());
         Thread gameThread = new Thread(gameInstance);
         gameThread.start();
         try {
@@ -28,28 +28,25 @@ public class Server {
 
             while(true) {
 
-                // connecting sockets
-                Socket firstPlayer = serverSocket.accept();
-                System.out.println("First client connected");
-                System.out.println("Waiting for the second player");
-
-                Socket secondPlayer = serverSocket.accept();
-                System.out.println("Second client connected");
-
                 try {
-                    OutputStream outputF = firstPlayer.getOutputStream();
-                    PrintWriter outF = new PrintWriter(outputF, true);
-                    System.out.println("Initializing player one");
+                    // connecting and initializing player one
+                    Socket firstPlayer = serverSocket.accept();
+                    BufferedReader inF = new BufferedReader(new InputStreamReader(firstPlayer.getInputStream()));
+                    PrintWriter outF = new PrintWriter(firstPlayer.getOutputStream(), true);
                     outF.println("1");
+                    System.out.println("First client connected");
+                    System.out.println("Waiting for the second player");
 
-                    OutputStream outputS = secondPlayer.getOutputStream();
-                    PrintWriter outS = new PrintWriter(outputS, true);
-                    System.out.println("Initializing player two");
+                    // connecting and initializing player two
+                    Socket secondPlayer = serverSocket.accept();
+                    BufferedReader inS = new BufferedReader(new InputStreamReader(secondPlayer.getInputStream()));
+                    PrintWriter outS = new PrintWriter(secondPlayer.getOutputStream(), true);
                     outS.println("2");
+                    System.out.println("Second client connected");
 
                     // game loop
                     while (true) {
-                        GameModeSelection gameModeSelection = new GameModeSelection(firstPlayer, secondPlayer);
+                        GameModeSelection gameModeSelection = new GameModeSelection(inF, inS, outF, outS);
                         Thread gameModeSelectionThread = new Thread(gameModeSelection);
                         gameModeSelectionThread.start();
                         try {
@@ -65,12 +62,12 @@ public class Server {
                             case BRAZILIAN -> {
                                 System.out.println("Selected brazilian");
                                 variantBuilder = new BrazilianVariantBuilder();
-                                startGame(firstPlayer, secondPlayer, variantBuilder);
+                                startGame(inF, inS, outF, outS, variantBuilder);
                             }
                             case POLISH -> {
                                 System.out.println("Selected polish");
                                 variantBuilder = new PolishVariantBuilder();
-                                startGame(firstPlayer, secondPlayer, variantBuilder);
+                                startGame(inF, inS, outF, outS, variantBuilder);
                             }
                             // TODO: third variant
                             case EXIT -> {
