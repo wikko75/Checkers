7795e4c02acff947bb7c158c2d553de866fdf7f4
Wiktor-Laptop <wiktorkar25@gmail.com>
2023-01-14 17:37:01 +0100

    Add docu for Board.java and Piece.java

--- src/main/java/com/example/checkers/game/Board.java
+++ src/main/java/com/example/checkers/game/Board.java
@@ -1,5 +1,7 @@
 package com.example.checkers.game;
 
+/**
+ * Class responsible for managing the game board*/
 public class Board {
 
     /**
@@ -9,8 +11,14 @@ public class Board {
      */
     protected final int size;
 
+    /**Pieces placed on the board and used throughout the game*/
     protected Piece[][] pieces;
 
+    /**
+     * Creates the game board, places pieces on their default positions
+     * @param size board size  \link size \endlink
+     * @param leftDownCornerBlack feature specifying board layout
+     */
     Board(int size, boolean leftDownCornerBlack) {
         this.size = size;
         pieces = new Piece[size][size];
@@ -33,21 +41,50 @@ public class Board {
         }
     }
 
+    /**
+     * Gets the size of the game board
+     * @return Board size*/
     public int getSize() {
         return size;
     }
+
+    /**
+     * Gets the piece from given board coordinates
+     * @param x  coordinate
+     * @param y  coordinate
+     * @return piece object at (x,y) */
     public Piece getPiece(int x, int y) {
         return pieces[x][y];
     }
 
+    /**
+     * Places the piece on the board at given coordinates
+     * @param x piece's coordinate
+     * @param y piece's coordinate
+     * @param color piece's color (WHITE / BLACK)
+     * @param type piece's type (MAN / KING)*/
     public void addPiece(int x, int y, Color color, Type type) {
         pieces[x][y] = new Piece(color, type);
     }
 
+    /**Removes the piece from the game board
+     * @param x piece's coordinate
+     * @param y piece's coordinate*/
     public void removePiece(int x, int y) {
         pieces[x][y] = null;
     }
 
+    /**
+     * Gets the board state in form of the String <br>
+     * Example: <br>
+     * "WM 00 WM 00 00 00 BM 00..." <br>
+     * W - white piece <br>
+     * B - black piece <br>
+     * M - man <br>
+     * K - king <br>
+     * 00 - no piece at all <br>
+     * @return String representation of the board state, column by column
+     * */
     public String getBoardState() {
         String boardState = "";
         for (int x=0; x<size; x++) {
--- src/main/java/com/example/checkers/game/Piece.java
+++ src/main/java/com/example/checkers/game/Piece.java
@@ -1,19 +1,33 @@
 package com.example.checkers.game;
-
+/**
+ * Class managing the piece of the game*/
 public class Piece {
 
+    /**Color of the piece @see ENUM Color*/
     private final Color color;
+
+    /**Type of the piece @see ENUM Type*/
     private final Type type;
 
+    /**
+     * Constructor - defines the color and the type of the piece instance
+     * @param color piece's color
+     * @param type  piece's type*/
     Piece(Color color, Type type) {
         this.color = color;
         this.type = type;
     }
 
+    /**
+     * Gets the color of the piece
+     * @return piece's color (WHITE / BLACK)*/
     Color getColor() {
         return color;
     }
 
+    /**
+     * Gets the type of the piece
+     * @return  piece's type (MAN / KING)*/
     Type getType() {
         return type;
     }
