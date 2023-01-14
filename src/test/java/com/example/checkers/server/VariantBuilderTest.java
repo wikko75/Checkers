a21b02b6e8ac4b38d5e01478afb3610a48a2a32d
JanNawrat <jan.ryszard.nawrat@gmail.com>
2023-01-14 13:46:31 +0100

    Removed test for deprecated BrazilianVariantBuilder, added tests for Italian and German VariantBuilders

--- src/test/java/com/example/checkers/server/VariantBuilderTest.java
+++ src/test/java/com/example/checkers/server/VariantBuilderTest.java
@@ -9,7 +9,8 @@ public class VariantBuilderTest {
     @Test
     public void testingVariantBuilder() {
         VariantBuilder polishVariantBuilder = new PolishVariantBuilder();
-        VariantBuilder brazilianVariantBuilder = new BrazilianVariantBuilder();
+        VariantBuilder italianVariantBuilder = new ItalianVariantBuilder();
+        VariantBuilder garmanVariantBuilder = new GermanVariantBuilder();
         VariantDirector variantDirector = new VariantDirector(polishVariantBuilder);
 
         String polishBoardState = "WM 00 WM 00 00 00 BM 00 BM 00";
@@ -23,22 +24,27 @@ public class VariantBuilderTest {
         polishBoardState = polishBoardState.concat(" WM 00 WM 00 00 00 BM 00 BM 00");
         polishBoardState = polishBoardState.concat(" 00 WM 00 WM 00 00 00 BM 00 BM ");
 
-        String brazilianBoardState = "WM 00 WM 00 00 00 BM 00";
-        brazilianBoardState = brazilianBoardState.concat(" 00 WM 00 00 00 BM 00 BM");
-        brazilianBoardState = brazilianBoardState.concat(" WM 00 WM 00 00 00 BM 00");
-        brazilianBoardState = brazilianBoardState.concat(" 00 WM 00 00 00 BM 00 BM");
-        brazilianBoardState = brazilianBoardState.concat(" WM 00 WM 00 00 00 BM 00");
-        brazilianBoardState = brazilianBoardState.concat(" 00 WM 00 00 00 BM 00 BM");
-        brazilianBoardState = brazilianBoardState.concat(" WM 00 WM 00 00 00 BM 00");
-        brazilianBoardState = brazilianBoardState.concat(" 00 WM 00 00 00 BM 00 BM ");
+        String italianBoardState = "00 WM 00 00 00 BM 00 BM";
+        italianBoardState = italianBoardState.concat(" WM 00 WM 00 00 00 BM 00");
+        italianBoardState = italianBoardState.concat(" 00 WM 00 00 00 BM 00 BM");
+        italianBoardState = italianBoardState.concat(" WM 00 WM 00 00 00 BM 00");
+        italianBoardState = italianBoardState.concat(" 00 WM 00 00 00 BM 00 BM");
+        italianBoardState = italianBoardState.concat(" WM 00 WM 00 00 00 BM 00");
+        italianBoardState = italianBoardState.concat(" 00 WM 00 00 00 BM 00 BM");
+        italianBoardState = italianBoardState.concat(" WM 00 WM 00 00 00 BM 00 ");
 
         Assertions.assertNotNull(variantDirector.getVariant());
         Assertions.assertEquals(variantDirector.getVariant().getBoardState(), polishBoardState);
 
-        variantDirector.setVariantBuilder(brazilianVariantBuilder);
+        variantDirector.setVariantBuilder(italianVariantBuilder);
 
         Assertions.assertNotNull(variantDirector.getVariant());
-        Assertions.assertEquals(variantDirector.getVariant().getBoardState(), brazilianBoardState);
+        Assertions.assertEquals(variantDirector.getVariant().getBoardState(), italianBoardState);
+
+        variantDirector.setVariantBuilder(garmanVariantBuilder);
+
+        Assertions.assertNotNull(variantDirector.getVariant());
+        Assertions.assertEquals(variantDirector.getVariant().getBoardState(), italianBoardState);
 
         variantDirector.setVariantBuilder(null);
 
