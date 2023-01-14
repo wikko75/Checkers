4d5281cc606417f3a30db9792b9b891c6f8dd98b
Wiktor-Laptop <wiktorkar25@gmail.com>
2023-01-14 17:43:55 +0100

    Add docu for Color.java and Type.java enums

--- src/main/java/com/example/checkers/game/Color.java
+++ src/main/java/com/example/checkers/game/Color.java
@@ -1,5 +1,7 @@
 package com.example.checkers.game;
-
+/**
+ *Enum specifying color of the piece<br>
+ * Values: WHITE / BLACK*/
 public enum Color {
     WHITE,
     BLACK
--- src/main/java/com/example/checkers/game/Type.java
+++ src/main/java/com/example/checkers/game/Type.java
@@ -1,5 +1,7 @@
 package com.example.checkers.game;
-
+/**
+ *Enum specifying type of the piece<br>
+ * Values: MAN / KING*/
 public enum Type {
     MAN,
     KING
