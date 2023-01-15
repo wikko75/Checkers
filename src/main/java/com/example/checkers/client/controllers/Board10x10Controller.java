eaf6099043deb4d7833e09b5df67f9fa4b3b2c2f
JanNawrat <jan.ryszard.nawrat@gmail.com>
2023-01-15 14:38:06 +0100

    Added Initialize method to Board10x10Controller to get access to all ImageView objects

--- src/main/java/com/example/checkers/client/controllers/Board10x10Controller.java
+++ src/main/java/com/example/checkers/client/controllers/Board10x10Controller.java
@@ -1,4 +1,29 @@
 package com.example.checkers.client.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class Board10x10Controller extends BoardController {
+    @FXML
+    private GridPane boardPane;
+    private ImageView[][] board;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        board = new ImageView[10][10];
+        for(Node node : boardPane.getChildren()) {
+            if(node!=null && GridPane.getColumnIndex(node)!=null && GridPane.getRowIndex(node)!=null) {
+                board[GridPane.getColumnIndex(node)][9 - GridPane.getRowIndex(node)] = (ImageView)node;
+            }
+        }
+        for(int i=0; i<10; i++) {
+            for(int j=0; j<10; j++) {
+                System.out.println(j+" "+i+" "+board[j][i].getId());
+            }
+        }
+    }
 }
