f1c0cfa3b6ad322635683e00482b6ca1bda29a23
JanNawrat <jan.ryszard.nawrat@gmail.com>
2023-01-15 16:45:50 +0100

    Added handling to board10x10 fields and sending move to server, removed old UI

--- src/main/java/com/example/checkers/client/controllers/Board10x10Controller.java
+++ src/main/java/com/example/checkers/client/controllers/Board10x10Controller.java
@@ -11,18 +11,12 @@ import java.util.ResourceBundle;
 public class Board10x10Controller extends BoardController {
     @FXML
     private GridPane boardPane;
-    private ImageView[][] board;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         board = new ImageView[10][10];
         for(Node node : boardPane.getChildren()) {
             if(node!=null && GridPane.getColumnIndex(node)!=null && GridPane.getRowIndex(node)!=null) {
                 board[GridPane.getColumnIndex(node)][9 - GridPane.getRowIndex(node)] = (ImageView)node;
-            }
-        }
-        for(int i=0; i<10; i++) {
-            for(int j=0; j<10; j++) {
-                System.out.println(j+" "+i+" "+board[j][i].getId());
             }
         }
     }
--- src/main/java/com/example/checkers/client/controllers/BoardController.java
+++ src/main/java/com/example/checkers/client/controllers/BoardController.java
@@ -1,12 +1,11 @@
 package com.example.checkers.client.controllers;
 
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
 public abstract class BoardController extends Controller implements Initializable{
@@ -14,26 +13,24 @@ public abstract class BoardController extends Controller implements Initializabl
     private Text board_state;
     @FXML
     private Label info;
-    @FXML
-    private TextField field;
-    @FXML
-    private Button send;
+    protected ImageView[][] board;
+    private boolean active;
+    private ImageView selected;
+    //private Image bf, bf_wm, bf_wk, bf_bm, bf_bk, wf, wf_wm, wf_wk, wf_bm, wf_bm;
     private boolean leftDownCornerBlack;
 
-    public void sendMove(ActionEvent event) {
-        String move = field.getText();
-        field.setText("");
-        if(move!=null) {
-            client.sendMessageToServer(move);
-        }
+    private void sendMove(String id1, String id2) {
+        String move = id1.charAt(1)+" "+id1.charAt(2)+" "+id2.charAt(1)+" "+id2.charAt(2);
+        client.sendMessageToServer(move);
+        System.out.println("Sending move: "+move);
     }
 
     public void moveRequest(boolean turn, String message) {
         if(turn) {
-            send.setDisable(false);
+            activate();
             Platform.runLater(() -> info.setText(message));
         } else {
-            send.setDisable(true);
+            deactivate();
             Platform.runLater(() -> info.setText("Opponent's turn"));
         }
     }
@@ -45,5 +42,38 @@ public abstract class BoardController extends Controller implements Initializabl
 
     public void setLeftDownCornerBlack(boolean leftDownCornerBlack) {
         this.leftDownCornerBlack = leftDownCornerBlack;
+    }
+
+    private  void activate() {
+        active = true;
+    }
+
+    private  void deactivate() {
+        active = false;
+    }
+
+    public void fieldClicked(MouseEvent event) {
+        if(active) {
+            ImageView clicked = (ImageView)event.getSource();
+            System.out.println("Clicked: "+clicked.getId());
+            if(selected==null) {
+                selected = clicked;
+                // TODO visible selection
+            } else if(selected==clicked) {
+                selected = null;
+                // TODO remove visible selection
+            } else {
+                sendMove(selected.getId(), clicked.getId());
+                selected = null;
+                // TODO remove visible selection
+            }
+            if(selected!=null) {
+                System.out.println("Selected not null");
+                System.out.println("Selected: "+selected.getId());
+            }
+            else {
+                System.out.println("Selected is null");
+            }
+        }
     }
 }
